2197f3d6a00bfedc645131852007ac694aed9394
max-debinsky <maksdeb.kontakt@gmail.com>
2026-06-12 22:32:27 +0200

    mvp

---
+++ build/CMakeFiles/demo.dir/link.d
@@ -0,0 +1,121 @@
+demo: \
+  /usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/crt1.o \
+  /usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/crti.o \
+  /usr/lib/gcc/x86_64-redhat-linux/16/crtbegin.o \
+  CMakeFiles/demo.dir/examples/demo.cpp.o \
+  libblit.a \
+  /usr/lib64/libSDL2-2.0.so.0.3200.68 \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libstdc++.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libm.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libm.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libm.so \
+  /lib64/libm.so.6 \
+  /lib64/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so \
+  /lib64/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libatomic_asneeded.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libatomic_asneeded.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libatomic_asneeded.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libatomic.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libatomic.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libatomic.so \
+  /usr/lib64/libatomic.so.1.2.0 \
+  /usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libc.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libc.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libc.so \
+  /lib64/libc.so.6 \
+  /usr/lib64/libc_nonshared.a \
+  /lib64/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so \
+  /lib64/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/16/libgcc.a \
+  /usr/lib/gcc/x86_64-redhat-linux/16/crtend.o \
+  /usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/crtn.o \
+  /lib64/ld-linux-x86-64.so.2
+
+/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/crt1.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/crti.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/crtbegin.o:
+
+CMakeFiles/demo.dir/examples/demo.cpp.o:
+
+libblit.a:
+
+/usr/lib64/libSDL2-2.0.so.0.3200.68:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libstdc++.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libm.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libm.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libm.so:
+
+/lib64/libm.so.6:
+
+/lib64/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so:
+
+/lib64/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libatomic_asneeded.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libatomic_asneeded.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libatomic_asneeded.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libatomic.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libatomic.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libatomic.so:
+
+/usr/lib64/libatomic.so.1.2.0:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libc.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libc.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/libc.so:
+
+/lib64/libc.so.6:
+
+/usr/lib64/libc_nonshared.a:
+
+/lib64/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libgcc_s.so:
+
+/lib64/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/libgcc.a:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/crtend.o:
+
+/usr/lib/gcc/x86_64-redhat-linux/16/../../../../lib64/crtn.o:
+
+/lib64/ld-linux-x86-64.so.2:
